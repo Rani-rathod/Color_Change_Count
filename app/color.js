2a0547d9785fbb680a718abf29bdee2100c05559
maggie <ranirathor20@navgurukul.org>
2023-08-09 14:59:28 +0530

    work on ui

---
+++ app/color.js
@@ -0,0 +1,15 @@
+
+const Color_Chenge = () => {
+    return(
+        <div className="box">
+            <div className="color-chenge"></div>
+            <div className="color-box"></div>
+            <div className="color-box2"></div>
+        </div>
+
+ 
+    )
+}
+export default Color_Chenge; 
+
+
---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Color_Chenge from './color';
+const App = () => {
+    return(
+        <>
+            <Color_Chenge />
+        </>
+    )
+}
+export default App;
